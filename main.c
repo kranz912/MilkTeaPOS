df759e0c76209746a8472555a3c9dc2f9e247958
kranz fontanilla <me@kranznikolai.com>
2019-11-18 15:07:49 -0500

    updated

--- main.c
+++ main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <string.h>
+#include<limits.h> 
+#define Classic_Pearl
+
+int  _MENU_INDEX = 0;
+struct MenuItem {
+    char Item[50];
+    float Price;
+};
+struct Menu {
+    struct MenuItem MenuItem[INT_MAX];
+};
 
 
+
+void Push(struct Menu *_menu,struct MenuItem item){
+    printf("%f",item.Price);
+    _menu->MenuItem[_MENU_INDEX] = item;
+    _MENU_INDEX +=1;
+}
 
 int main(){
+    struct Menu *Menu;
+
+    printf("%f",Menu->MenuItem[0].Price);
     printf("hello world");
 }
